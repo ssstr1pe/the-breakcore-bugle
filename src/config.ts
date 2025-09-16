f2004d3771896280b3d1f22db9d9813dc8c48aba
ssstr1pe <str1pe.c0re.1@gmail.com>
2025-09-16 23:29:23 +0100

    maybe this?

--- src/config.ts
+++ src/config.ts
@@ -59,11 +59,11 @@ export const profileConfig: ProfileConfig = {
 	],
 };
 
-// export const licenseConfig: LicenseConfig = {
-// 	enable: true,
-// 	name: "CC BY-NC-SA 4.0",
-// 	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
-// };
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
